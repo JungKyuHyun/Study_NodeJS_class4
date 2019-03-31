066420140bf16f42d5fe393b587fe5373ae07640
BokDaBeen <ekqlszzzz@naver.com>
2019-03-31 23:18:03 +0900

    Update Mission_dabeen(second).js
    두번째 올렸던거 좀더 수정했어요

--- 20190331/DABEEN/Mission_dabeen(second).js
+++ 20190331/DABEEN/Mission_dabeen(second).js
@@ -1,6 +1,3 @@
-
-
-
 function user(name,age,phoneNum) {
     this.name = name;
     this.age = age;
@@ -9,17 +6,29 @@ function user(name,age,phoneNum) {
 };
 
 //var Users = [{user1}, {user2}, {user3}];
-var Users = [new user('김동현',25,'010-1111-2222'),
+var users = [new user('김동현',25,'010-1111-2222'),
              new user('장지훈',27,'010-3333-4444'),
              new user('복다빈',27,'010-9510-0730')];
 
 
+function getUserInfo(users){
+    if (users && users.length <= 0) {
+        return '';
+    }
+
+    var result = '';
+    for (var i = 0 ; i < users.length ; i++) {
+        result += users[i].name + ' ' + users[i].age + ' ' + users[i].phoneNum ;
+        if( i !== users.length-1 ){
+            result += '\n';
+        }
+    }
+
+    return result;
+}
 
 var fs=require('fs');
-fs.writeFile('./output.txt',Users[0].name+" "+Users[0].age+" "+Users[0].phoneNum+"\n"+
-                            Users[1].name+" "+Users[1].age+" "+Users[1].phoneNum+"\n"+
-                            Users[2].name+" "+Users[2].age+" "+Users[2].phoneNum
-                            ,function(err){
+fs.writeFile('./output.txt', getUserInfo(users), function(err){
     if(err) console.log(err);
 });
 
@@ -27,8 +36,8 @@ fs.writeFile('./output.txt',Users[0].name+" "+Users[0].age+" "+Users[0].phoneNum
 fs.readFile('./output.txt','utf8',function(err,data){
     if(err) throw err;
     var UsersInfo = []; 
-    UsersInfo = data.toString().split("\n");
-    for(i in UsersInfo){
-        console.log(UsersInfo[i].split(" ",1));
+    usersInfo = data.toString().split("\n");
+    for(i in usersInfo){
+        console.log(usersInfo[i].split(" ",1));
     }
 });
